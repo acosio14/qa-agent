0787e9a82870fb29c9b05ec193d3b06f37812f5c
Adrian Cosio <adriancosio14@gmail.com>
2026-07-06 05:59:51 -0500

    feat: add initial file parser functionality.

--- src/qa-agent-cli/file_parser.py
+++ src/qa-agent-cli/file_parser.py
@@ -1,4 +1,22 @@
 # Parses the terminal inputs (takes and structures it to correct format)
+from pathlib import Path
+import logging
 
-def parse():
-    ...
+def parse(filepath: Path) -> dict[str]:
+    # Need to verify if its a directory or file
+    # if directory, then loop through each file and make dict of each
+    if Path(filepath).is_file():
+        files = [filepath] # should be a file
+    elif Path(filepath).is_dir():
+        files = [file for file in Path(filepath).iterdir()]
+    else:
+        logging.error('File path cant be read as file or dir')
+
+    file_dict = {}
+    for file in files:
+        with open(file) as f:
+            # Read file line by line and convert to dictionary
+            content = f.read()
+            file_dict[file] = content
+
+    return file_dict

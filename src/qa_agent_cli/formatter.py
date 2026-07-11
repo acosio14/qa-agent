23a01555a778e6470ca8742baa8a9884ce88b6ff
Adrian Cosio <adriancosio14@gmail.com>
2026-07-11 06:19:43 -0500

    feat: complete end-to-end Q&A agent.

--- src/qa_agent_cli/formatter.py
+++ src/qa_agent_cli/formatter.py
@@ -1,3 +1,12 @@
 
-def format():
-    ...
+def format(files: dict[str], question: str) -> str:
+    for file in files:
+        if file == "":
+            raise FileNotFoundError("File has no content. Empty value.")
+
+    prompt_files = f"Files: ```{files}```"
+    prompt_question = f"Question: ```{question}```"
+
+    print(prompt_files + "\n" + prompt_question)
+
+    return prompt_files + prompt_question
--- src/qa_agent_cli/main.py
+++ src/qa_agent_cli/main.py
@@ -7,23 +7,29 @@ def main():
         description="Q&A Agent - Will answer questions about any file."
     )
 
-    parser.add_argument("path") # filepath or dir_path
-    parser.add_argument("question")
+    parser.add_argument("path", type=str) # filepath or dir_path
+    parser.add_argument("question", type=str)
     parser.add_argument("--model") #optional
 
     args = parser.parse_args()
     
     free_models = {
-        "gemma-4-31b": "google/gemma-4-31b-it:free", 
-        "minstral_7b": "minstralai/mistral-7b-instruct:free"
+        "gemma-4-31b": "google/gemma-4-31b-it:free",
+        "gemma-4-26b": "google/gemma-4-26b-a4b-it:free",
+        "gpt-oss-120": "openai/gpt-oss-120b:free",
+        "gpt-oss-20b": "openai/gpt-oss-20b:free",
+        "nemotron-3-ultra": "nvidia/nemotron-3-ultra-550b-a55b:free",
+        "nemotron-3-nano-omni": "nvidia/nemotron-3-nano-omni-30b-a3b-reasoning:free",
     }
     model = free_models["gemma-4-31b"]
     if args.model:
         model = free_models.get(args.model)
 
-    dict_variable = file_parser.parse(args.path)
-    formatted_question = formatter.format(dict_variable, args.question)
-    llm.QAAssistant(formatted_question, model).GetAnswer()
+    files_dict = file_parser.parse(args.path)
+    formatted_question = formatter.format(files_dict, args.question)
+    response = llm.QAAssistant(formatted_question, model).GetAnswer()
+
+    print(response)
     
 
 

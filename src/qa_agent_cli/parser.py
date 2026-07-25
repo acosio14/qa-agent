7220e8b81c19ecce3c6bbe4941fc54bdf5e68a00
Adrian Cosio <adriancosio14@gmail.com>
2026-07-25 06:03:29 -0500

    refactor: remove registry pattern and convert all file to markdown

--- src/qa_agent_cli/parser.py
+++ src/qa_agent_cli/parser.py
@@ -6,87 +6,35 @@
 from pathlib import Path
 import logging
 from file_types import ParsedFile
-from charset_normalizer import detect
 from docx import Document
-import fitz
+from markitdown import MarkItDown
 
-PARSER_REGISTRY = {}
 
-def register_parser(file_type):
-    def decorator(func):
-        PARSER_REGISTRY[file_type] = func
-        return func
-    return decorator
+def convert_to_markdown(filepath):
+    md = MarkItDown(enable_plugins=False)
+    md_text = md.convert(filepath)
+    return md_text
 
-@register_parser(".txt")
-def txt_file_parser(filepath):
-    with open(filepath, "rb") as f:
-        file_bytes = f.read()
-    file_encoding = detect(file_bytes)["encoding"]
-    file_content = file_bytes.decode(file_encoding)
-    chunks = "" # if file < some_size => don't chunk
-    return file_content, chunks # chunk by line numbers?
-
-@register_parser(".md")
-def markdown_parser(filepath):
-    with open(filepath, "r") as f:
-        file_content = f.read()
-    return file_content # Chunk by section heading
-
-@register_parser(".docx") # pages, section = paragraph?
-def docx_parser(docx_filepath):
-    doc = Document(docx_filepath)
-    file_content = []
-
-    for p in doc.paragraphs:
-        text = p.text.strip()
-        if not text:
-            continue
-
-        style_name = p.style.name.lower()
-
-        # Handle Headings
-        if "heading 1" in style_name:
-            file_content.append(f"# {text}")
-        elif "heading 2" in style_name:
-            file_content.append(f"## {text}")
-        elif "heading 3" in style_name:
-            file_content.append(f"### {text}")
-
-        # Handle Bullet Points / Lists
-        elif "list bullet" in style_name:
-            file_content.append(f"* {text}")
-        elif "list number" in style_name:
-            file_content.append(f"1. {text}")
-
-        # Handle Normal Paragraphs
-        else:
-            file_content.append(text)
-
-    return "\n\n".join(file_content)
-
-@register_parser(".pdf")
-def pdf_parser(file):
-    doc = fitz.open(file)
-    for page in doc:
-        text = page.get_text()
-        # pages, sections = paragraphs?
+def chunk_file_content(markdown_text: str):
+    ...
 
 def create_file_type_dataclass(filepath: Path):
-    # Verify if it a supported file type and parse it.
-    # Supported types: .txt, .md, .csv, .docx, .pdf,
+    
     filepath = Path(filepath)
     file_extension = filepath.suffix
     filename = filepath.stem
 
-    # Use specific file ext parser function in registry
-    try:
-        file_parser = PARSER_REGISTRY.get(file_extension)
-        raw_text, chunks = file_parser(filepath)
-    except:
+    # Verify if file type supported
+    supported_file_types = [".txt", ".pptx", ".docx", ".xlsx", ".pdf", ".md"]
+    if file_extension not in supported_file_types:
         TypeError(f"Don't support file extension: {file_extension}")
-    
-    return ParsedFile(filename, file_extension, raw_text, chunks)
+
+    md_text = convert_to_markdown(filepath)
+    # if len(md_text) > some_size:
+    chunks =  chunk_file_content(md_text)
+    # else { no chunks }
+
+    return ParsedFile(filename, file_extension, md_text, chunks)
         
 
 def parse(filepath: Path) -> dict[str]:

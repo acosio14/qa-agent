7fc43b42aed64fb37bf24dabb6d014b3f2106bd3
Adrian Cosio <adriancosio14@gmail.com>
2026-08-05 06:23:48 -0500

    feat: add top k chunk retriever with bm25 algo

---
+++ src/qa_agent_cli/retriever.py
@@ -0,0 +1,20 @@
+# Takes question and stored chunks -> creates top-k chunks
+from qa_agent_cli.file_types import ParsedFile, Chunk
+from dataclasses import asdict
+import bm25s
+
+
+def retrieve_top_k_chunks(k: int, parsed_files: list[ParsedFile], question: str) -> list[ParsedFile]:
+
+
+    question_tokens = bm25s.tokenize(question)
+    
+    chunks = [asdict(file.chunks) for file in parsed_files]
+    chunk_tokens = bm25s.tokenize(chunk["text"] for chunk in chunks)
+
+    retriever = bm25s.BM25(corpus=chunks)
+    retriever.index(chunk_tokens)
+
+    chunk_dict, score = retriever.retrieve(question_tokens, k=2)
+
+

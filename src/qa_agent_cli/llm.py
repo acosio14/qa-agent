2974eb36726a4ee463ec5d8f342c997d5aa3a76a
Adrian Cosio <adriancosio14@gmail.com>
2026-08-06 06:14:46 -0500

    feat: add try/except to catch TooManyRequestsResponseError.

--- src/qa_agent_cli/llm.py
+++ src/qa_agent_cli/llm.py
@@ -1,4 +1,5 @@
 from openrouter import OpenRouter
+import openrouter.errors as errors
 import os
 from dotenv import load_dotenv
 
@@ -19,12 +20,19 @@ class QAAssistant:
     def GetAnswer(self) -> str:
 
         with OpenRouter(api_key=os.getenv("OPENROUTER_API_KEY")) as open_router:
-            response = open_router.chat.send(
-                model=self.model,
-                messages=[
-                    {"role": "system", "content": self.system_prompt}, 
-                    {"role": "user", "content": self.user_prompt},
-                ]
-            )
+            try:
+                response = open_router.chat.send(
+                    model=self.model,
+                    messages=[
+                        {"role": "system", "content": self.system_prompt}, 
+                        {"role": "user", "content": self.user_prompt},
+                    ]
+                )
 
-            return response.choices[0].message.content
+                return response.choices[0].message.content
+            except errors.TooManyRequestsResponseError as e:
+                print("status:", getattr(e, "status_code", None))
+                print("body:", getattr(e, "body", None))
+                print("message:", getattr(e, "message", None))
+                print("raw:", getattr(e, "raw_response", None))
+                print("dir:", [a for a in dir(e) if not a.startswith("_")])
